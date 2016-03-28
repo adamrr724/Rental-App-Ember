a64c3afa4b6a1b5f6f3198c95a3c481cf7c57b73
adam ross russell and jon sweeney <adam+jon@>
2016-03-28 11:35:48 -0700

    add more css to rental images

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -2,12 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isImageShowing: false,
+  isBigImageShowing: true,
   actions: {
     imageShow: function() {
       this.set('isImageShowing', true);
     },
     imageHide: function() {
       this.set('isImageShowing', false);
+    },
+    bigImageHide: function() {
+      this.set('isBigImageShowing', false);
     },
     delete(rental) {
       if (confirm('Are you sure you want to delete this rental?')) {
