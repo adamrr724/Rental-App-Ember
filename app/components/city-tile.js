3f612d24788a592ad17ac420d379f144ca8870d6
Jon Nardine and adam ross russell <Guest@epicodus-33.local>
2016-04-04 12:00:01 -0700

    trying to implement sort select

--- app/components/city-tile.js
+++ app/components/city-tile.js
@@ -5,9 +5,23 @@ export default Ember.Component.extend({
     return this.get('city.name') + ", " + this.get('city.country');
   }),
 
+  sortChoice: Ember.computed( function() {
+    var choice = this.get('sortedBy');
+    console.log(choice);
+
+    return "{{#each " + choice + " as |rental|}}" +
+      "<li>{{rental.owner}}s {{rental.type}}</li>" +
+      "<li>{{rental-cost rental}}</li>" +
+      "<li><img src='{{rental.image}}' /></li>" +
+    "{{/each}}"
+  }),
+
   sortBy: ['cost:asc'],
   sortedRentals: Ember.computed.sort('city.rentals', 'sortBy'),
-  
+
+  sortBy: ['bedrooms:asc'],
+  sortedBedrooms: Ember.computed.sort('city.rentals', 'sortBy'),
+
   actions: {
     destroyCity(city) {
       var rental_deletions = city.get('rentals').map(function(rental) {
