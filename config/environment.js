43d953a0f13bf9c45a4292bcbe648381a558e0f7
VC <vconarro33@gmail.com>
2016-05-05 16:15:42 -0700

    add dist files for ghpages launch

--- config/environment.js
+++ config/environment.js
@@ -51,7 +51,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.baseURL = '/GITHUB-REPO-NAME/dist';
+    ENV.baseURL = '/rental-app-ember/dist';
     ENV.locationType = 'hash';
   }
 
